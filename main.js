c2ea1f9b2df6db3b092f222b17c0030635b74efe
ayub432 <ayubmars9@gmail.com>
2024-11-09 18:07:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let ism = prompt('string typdagi malumot kiriting');
+
+
+let son = prompt('Number typedagi malumot kiriting');
+
+
+let boolean = prompt('boolean typdagi malumot kiriting');
+
+
+let ok = confirm('hamma malumotlarni togri kiritganizga ishonchingiz komilmi')
+
+let xabar = `Siz kiritgan ${ism} so'zi string typega tegishli
+Siz kiritgan ${son} so'zi number typega tegishli
+Siz kiritgan ${boolean} so'zi boolean typega tegishli`;
+console.log(xabar, ism, son, boolean);
+alert(xabar);
